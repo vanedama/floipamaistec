818adb786839001efb3b1d0907c23b8bc5341937
vanedama <vanessadama10@yahoo.com>
2023-03-01 09:12:14 -0300

    Primeiros exercícios

---
+++ Módulo 01/Semana 01/caculadora/caculadora/Program.cs
@@ -0,0 +1,46 @@
+﻿// Importação de bibliotecas
+
+// Nome da classe
+
+// Declaração de método
+
+// Declaração de variáveis
+int opção, numero1 = 0, numero2 = 0;
+float resultado = 0;
+
+    // Entradas
+Console.WriteLine("Escolha uma operação: \n\n" +
+    "1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n" +
+    "5 - sair\n\nDigite a opção escolhida: ");
+
+opção = int.Parse  (Console.ReadLine());
+
+    // Processamento
+    switch(opção)
+{
+    // Bloco de instruções da estrutura condicional
+    case 1: 
+        Console.WriteLine("Operação somar.");
+        Console.Write("Digite o número 1: ");
+        numero1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o número 2: ");
+        numero2 = int.Parse(Console.ReadLine());
+        resultado = numero1 + numero2;
+        break;
+    case 2:
+        Console.WriteLine("Operação subtrair.");
+        break;
+    case 3:
+        Console.WriteLine("Operação multiplicar.");
+        break;
+    case 4:
+        Console.WriteLine("Operação dividir.");
+        break;
+    case 5:
+        Console.WriteLine("Saindo do sistema.");
+        break;
+}
+    Console.WriteLine("Resultado: " + resultado);
+
+    // Saídas
+    
---
+++ Módulo 01/Semana 01/ola_Mundo/ola_Mundo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Comentário
+Console.WriteLine("Olá, Mundo!");
+
+int numeroInteiro = 10; //Declaração de variável inteira, recebendo valor 10
+long numeroMaior = 1099999L; // Declaração de variável long, recebendo valor 1099999
+float numeroQuebrado = 6.8f; // Declaração de variável do tipo float
+double numeroQuebradoMaior = 101.999999D; // Declaração de variável do tipo double
+char letra = 'z'; // Declaração de variável do tipo char (1 caracter)
+string texto = "A turma Estreito é a melhor."; // Declaração de variável do tipo string, recebendo uma frase
+bool variavelBooleana = true; // Declaração de variável do tipo boolean, recebendo valor verdadeiro
+DateTime data = DateTime.Now; // Declaraçã de variável do tipo DateTime (data até segundos), armazenando os segundo atuais
+DateTime outraData = new DateTime(2023, 01, 31); // Instanciando objeto da classe DateTime, passando parâmetros como mês e dia
+texto.ToUpper(); // Convertendo o texto para letras maiúsculas
+Console.WriteLine("Frase com letras maiúsculas: " + texto.ToUpper());
+
+string nomeCompleto; // Declaração de variável
+Console.WriteLine("Digite o seu nome: "); // Instrução ao usuário, impressão em tela
+nomeCompleto = Console.ReadLine(); // Aguardando o usuário digitar no terminal
+Console.WriteLine("O seu nome é: " + nomeCompleto); // Saída de dados, concatenando texto com valor da variável
